615b7732f7211e8fae412674c387f30946304655
Andrei Jitea <andrei_petru.jitea@stud.acs.upb.ro>
2024-11-11 22:30:23 +0200

    Adaugat punctul 366

--- l5/main.py
+++ l5/main.py
@@ -17,7 +17,11 @@ for j, i in enumerate(["578", "546", "436", "405"]):
     wave_len = int(i) * 1e-9 # m
     frecv.append(c / wave_len) # Hz
     U_med.append(np.mean((data[:]))) # V
-    #print(f"U_med_{i} = {U_med[j]:.3f}, frecv_{i} = {frecv[j]:.3e}")
+
+# Procesez datele pentru 366 nm (exclus din regresie)
+wave_len_366 = 366 * 1e-9 # m
+frecv_366 = c / wave_len_366 # Hz
+U_med_366 = np.mean(np.genfromtxt("data_366.csv", delimiter=",", skip_header=1)) # V
 
 # Plot
 fig, ax = plt.subplots()
@@ -33,6 +37,8 @@ ax.plot(frecv, p(frecv), linestyle='--')
 h = coef[0] * e
 print(f"Plank: {h:.3e} J/Hz")
 ax.plot([], [], ' ', label=f"Plank: {h:.3e} J/Hz")
+# Afisez valoarea pentru 366 nm
+ax.plot(frecv_366, U_med_366, 'bo')
 
 # Calculez frecventa de prag si lucrul mecanic de extractie
 frecv_prag = -coef[1] / coef[0]
